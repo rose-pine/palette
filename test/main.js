1139c2911b05aa27b263713bc0731549fafdb5f0
mvllow <mvllow@icloud.com>
2023-03-29 22:02:52 -0500

    test: refactor colour formats

---
+++ test/main.js
@@ -0,0 +1,21 @@
+import test from 'ava'
+import {variants, roles} from '../source/index.js'
+
+test('main', (t) => {
+	t.deepEqual(roles.base, {
+		main: {hex: '191724', rgb: [25, 23, 36], hsl: [249, 22, 12]},
+		moon: {hex: '232136', rgb: [35, 33, 54], hsl: [246, 24, 17]},
+		dawn: {hex: 'faf4ed', rgb: [250, 244, 237], hsl: [32, 57, 95]},
+	})
+
+	t.deepEqual(variants.dawn.highlightLow, {
+		hex: 'f4ede8',
+		rgb: [244, 237, 232],
+		hsl: [25, 35, 93],
+		alpha: {
+			hex: '6e6a860d',
+			rgb: [110, 106, 134, 0.05],
+			hsl: [249, 12, 47, 0.05],
+		},
+	})
+})
