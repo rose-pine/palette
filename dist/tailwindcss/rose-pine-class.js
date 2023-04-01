d6d847d57987d12b2ebfcb7794edf5cec98adfc0
mvllow <mvllow@icloud.com>
2023-03-31 19:10:23 -0500

    feat: add extra tailwind templates
    This commit refactors the build script to be more functional and easier
    to maintain.

---
+++ dist/tailwindcss/rose-pine-class.js
@@ -0,0 +1,24 @@
+export default {
+	"darkMode": [
+		"class"
+	],
+	"theme": {
+		"colors": {
+			"rp-base": "hsl(--rp-base / <alpha-value>)",
+			"rp-surface": "hsl(--rp-surface / <alpha-value>)",
+			"rp-overlay": "hsl(--rp-overlay / <alpha-value>)",
+			"rp-muted": "hsl(--rp-muted / <alpha-value>)",
+			"rp-subtle": "hsl(--rp-subtle / <alpha-value>)",
+			"rp-text": "hsl(--rp-text / <alpha-value>)",
+			"rp-love": "hsl(--rp-love / <alpha-value>)",
+			"rp-gold": "hsl(--rp-gold / <alpha-value>)",
+			"rp-rose": "hsl(--rp-rose / <alpha-value>)",
+			"rp-pine": "hsl(--rp-pine / <alpha-value>)",
+			"rp-foam": "hsl(--rp-foam / <alpha-value>)",
+			"rp-iris": "hsl(--rp-iris / <alpha-value>)",
+			"rp-highlight-low": "hsl(--rp-highlight-low / <alpha-value>)",
+			"rp-highlight-med": "hsl(--rp-highlight-med / <alpha-value>)",
+			"rp-highlight-high": "hsl(--rp-highlight-high / <alpha-value>)"
+		}
+	}
+}
